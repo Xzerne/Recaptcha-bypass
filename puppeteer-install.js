e39bc6dde19ce0d2fd669ca8f1266d2974b692a5
Kujou Kurumi <167000432+Xzerne@users.noreply.github.com>
2024-09-09 15:13:00 +0700

    Update puppeteer-install.js

--- puppeteer-install.js
+++ puppeteer-install.js
@@ -1,12 +1,1 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-  try {
-    // Launch Puppeteer to trigger Chromium download
-    await puppeteer.launch();
-    console.log('Chromium installation successful');
-  } catch (error) {
-    console.error('Chromium installation failed:', error);
-    process.exit(1);
-  }
-})();
+// hi
